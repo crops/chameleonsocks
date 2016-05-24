ee1729e36e0aff4d3f8fb328f4247a1376268678
Todor Minchev <todor.minchev@linux.intel.com>
2016-05-24 13:33:15 -0700

    pac.py: add Intel copyright notice
    Signed-off-by: Todor Minchev <todor.minchev@linux.intel.com>

--- confs/pac.py
+++ confs/pac.py
@@ -1,3 +1,18 @@
+#
+# Copyright (C) 2016 Intel Corporation
+#
+# Author: Todor Minchev <todor.minchev@linux.intel.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU General Public License,
+# version 2, or (at your option) any later version, as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+
 import fileinput
 import re
 import iptools
